d3a065939e92e78ed37f12ca64fd6e0d70bdc176
omer <jagenau49@gmail.com>
2022-06-23 12:22:37 +0200

    index about and blog pages are in

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const host = "127.0.0.1";
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./site/index.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve("./site/about.html"));
+});
+app.get("/blog", (req, res) => {
+  res.sendFile(path.resolve("./site/blog.html"));
+});
+
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
