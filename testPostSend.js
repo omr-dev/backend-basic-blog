4e0aabb62e7b1078df5a9bff8ab4f3e5b7b5c38e
omer <jagenau49@gmail.com>
2022-06-23 19:08:28 +0200

    crud tests finished

--- testPostSend.js
+++ testPostSend.js
@@ -3,13 +3,40 @@ import { postModel } from "./models/Post.js";
 
 mongoose.connect("mongodb://localhost:27017/nodeblog_test_db");
 
-postModel.create(
-  { title: "Test Post", content: "This is a test post" },
-  (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(result);
-    }
+postModel.findByIdAndDelete("62b49880bfc1fd2c6ce53bc9", (err, post) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
   }
-);
+});
+// postModel.findByIdAndUpdate(
+//   "62b49880bfc1fd2c6ce53bc9",
+//   { title: "this an updated second title" },
+//   (err, posts) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(posts);
+//     }
+//   }
+// );
+
+// postModel.find({title:'Test Post'}, (err, posts) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// postModel.create(
+//   { title: "Test Post 2", content: "This is second test post" },
+//   (err, result) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log(result);
+//     }
+//   }
+// );
