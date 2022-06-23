dd590c60e49c2edc8be6530011440f4babdfd95c
omer <jagenau49@gmail.com>
2022-06-23 12:54:31 +0200

    html pages converted to handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import path from "path";
 import express from "express";
+import { engine } from "express-handlebars";
 
 const app = express();
 const port = 3000;
@@ -7,15 +8,24 @@ const host = "127.0.0.1";
 
 app.use(express.static("public"));
 
+//handlebars installation
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./site/index.html"));
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve("./site/about.html"));
+  res.render("about");
 });
 app.get("/blog", (req, res) => {
-  res.sendFile(path.resolve("./site/blog.html"));
-});
+    res.render("blog");
+  });
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve("./site/index.html"));
+// });
+
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
